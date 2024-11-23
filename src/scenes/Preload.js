96b1a477cfe5f26265edb6853553eadcdd1d6723
Sarah Rosanna Busch <annarose0113@gmail.com>
2024-11-23 12:21:49 -0800

    endzone and scoreboard

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -26,21 +26,21 @@ export default class Preload extends Phaser.Scene {
 	editorCreate() {
 
 		// progressBar
-		const progressBar = this.add.rectangle(553, 361, 256, 20);
+		const progressBar = this.add.rectangle(497, 361, 256, 20);
 		progressBar.setOrigin(0, 0);
 		progressBar.isFilled = true;
 		progressBar.fillColor = 14737632;
 
 		// progressBarBg
-		const progressBarBg = this.add.rectangle(553.0120849609375, 361, 256, 20);
+		const progressBarBg = this.add.rectangle(497, 361, 256, 20);
 		progressBarBg.setOrigin(0, 0);
 		progressBarBg.fillColor = 14737632;
 		progressBarBg.isStroked = true;
 
 		// loadingText
-		const loadingText = this.add.text(552.0120849609375, 329, "", {});
+		const loadingText = this.add.text(497, 329, "", {});
 		loadingText.text = "Loading...";
-		loadingText.setStyle({ color: "#e0e0e0", fontFamily: "arial", fontSize: "20px" });
+		loadingText.setStyle({ "color": "#e0e0e0", "fontFamily": "arial", "fontSize": "20px" });
 
 		this.progressBar = progressBar;
 
