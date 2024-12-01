9e7eb27b3696d5cfde36c3a38a433ac2a8d3cb15
Sarah Rosanna Busch <annarose0113@gmail.com>
2024-12-01 12:11:16 -0800

    error message

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Level from "./scenes/Level.js";
 import Preload from "./scenes/Preload.js";
 
+window.onerror = (msg, src, line, col, e) => {
+	console.log("couldn't load game :(");
+	const errorContainer = document.getElementById('error-container'); 
+	errorContainer.innerHTML = `<div class="error-message">
+	<p>Oops, something went wrong :(</p>
+	<p>${msg}</p>
+	</div>`;
+}
+
 window.addEventListener('load', function () {
 
 	var game = new Phaser.Game({
 		height: 256,
 		width: 6724,
 		type: Phaser.AUTO,
-        backgroundColor: "#000000",
+		backgroundColor: "#000000",
 		scale: {
 			mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH
 		},
@@ -31,6 +40,7 @@ class Boot extends Phaser.Scene {
 	preload() {
 		
 		//this.load.pack("pack", "assets/preload-asset-pack.json");
+		console.log("You came here looking for cheat codes, didn't you? Good thinking. I'm sure I saw them around here somewhere ;)")
 	}
 
 	create() {
